da5fac9ccf154715caddd06431238b548d1d0b25
born2bramble <born2bramble@gmail.com>
2025-09-08 13:21:52 +0100

    Update nit.user.js

--- nit.user.js
+++ nit.user.js
@@ -2,8 +2,8 @@
 // @name         Get Tags
 // @namespace    http://tampermonkey.net/
 // @version      2024-09-13
-// @updateURL    
-// @downloadURL  
+// @updateURL    https://github.com/born2bramble/nit/raw/refs/heads/main/nit.user.js
+// @downloadURL  https://github.com/born2bramble/nit/raw/refs/heads/main/nit.user.js
 // @description  try to take over the world!
 // @author       You
 // @match        https://beta.nomi.ai/nomis
